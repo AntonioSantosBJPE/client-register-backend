76fec455cb61b4799da193efe13548e580d47a7a
Antonio <netoifpe@gmail.com>
2023-05-22 12:20:22 -0300

    feat: creates the entities Client and Contact

---
+++ src/entities/client.entitie.ts
@@ -0,0 +1,40 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import { Contact } from "./contact.entitie";
+
+@Entity("clients")
+export class Client {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column({ type: "varchar", length: "150" })
+  name: string;
+
+  @Column({ type: "varchar", length: "45", unique: true })
+  email: string;
+
+  @Column({ type: "varchar", length: "11" })
+  phone: string;
+
+  @Column({ type: "varchar", length: "120" })
+  password: string;
+
+  @CreateDateColumn({ type: "date" })
+  createdAt: string;
+
+  @UpdateDateColumn({ type: "date" })
+  updatedAt: string;
+
+  @DeleteDateColumn({ type: "date" })
+  deletedAt: string;
+
+  @OneToMany(() => Contact, (contact) => contact.client)
+  contacts: Contact[];
+}
