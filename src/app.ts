a3a9c4e66b16107af92f27b0cb7ee126d394e9a0
Antonio <netoifpe@gmail.com>
2023-05-22 11:38:09 -0300

    feat:configuring the cors in the application

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import "express-async-errors";
 import express, { Application } from "express";
 import { errorHandler } from "./error";
+import cors from "cors";
 
 const app: Application = express();
 
 app.use(express.json());
-
+app.use(cors());
 app.use(errorHandler);
 
 export default app;
