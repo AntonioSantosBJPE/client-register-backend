4dab11896485591b97be4502c9b8f9765cca9f55
Antonio <netoifpe@gmail.com>
2023-05-22 11:32:44 -0300

    feat: create class for global error handling

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import "express-async-errors";
 import express, { Application } from "express";
-//import { errorHandler } from "./errors";
+import { errorHandler } from "./error";
 
 const app: Application = express();
 
 app.use(express.json());
 
-//app.use(errorHandler);
+app.use(errorHandler);
 
 export default app;
