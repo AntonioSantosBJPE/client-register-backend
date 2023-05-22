0f2c6b56bd6f2b24f6ccb784274a37ba79ab288b
Antonio <netoifpe@gmail.com>
2023-05-22 12:54:51 -0300

    feat: create the clients route controllers

---
+++ src/controllers/client.contoller.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from "express";
+
+export const createClientController = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  return res.status(201).json("Route create new client!");
+};
+
+export const retrieveListClientController = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  return res.status(200).json("Route retrieve list of client!");
+};
+
+export const retrieveClientController = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  return res.status(200).json("Route retrieve client!");
+};
+
+export const updateClientController = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  return res.status(200).json("Route update client!");
+};
+
+export const deleteClientController = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  return res.status(204);
+};
